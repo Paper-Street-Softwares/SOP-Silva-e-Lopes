5e8d2df91c93937ab3c58157a21758a8d6243b55
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-02-05 14:42:46 -0300

    fix: Logo

--- src/components/sections/NavbarNovaTemplate.jsx
+++ src/components/sections/NavbarNovaTemplate.jsx
@@ -89,7 +89,7 @@ function NavbarNovaTemplate({
               alt={content.texts.navbar.logo.alt}
               className="w-[100%] hidden desktop1:flex"
               width={350}
-              height={370}
+              height={170}
               fetchPriority="high"
             />
           </div>
